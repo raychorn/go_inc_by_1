7b3a5aae296b01fced83811a1d88ab55dc60782a
Ray C Horn <raychorn@gmail.com>
2020-10-08 18:19:38 -0600

    THis is the final code.  State machine with optional odometer rollover.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 (c). Ray C Horn, All RIghts Reserved.
 
+This is a simple state-machine where the first digit on the right is increments by 1 followed-by 
+rippling the addition to the next digit if necessary with the edge condition being the last
+digit to the left being reset on carry from greater than 9.  Notice the option to roll the
+odometer or not which was added after the code had been  written.
+
--- increment_by1.go
+++ increment_by1.go
@@ -1,10 +1,41 @@
 package main
 
-func inc_by_1(items int[]) int[] {
-	fmt.Println(items)
+import (
+    "fmt"
+)
+
+type options struct {
+    rollover bool
+}
+
+func incby1(items []int, opt options) []int {
+    j := 0;
+    n := len(items)-1
+    for i := n; i > 0; i-- {
+        if (j == 0) {
+            items[i]++
+        }
+        if (items[i] > 9) {
+            items[i] = 0
+            items[i-1]++
+            if (items[0] > 9) {
+                items[0] = 0
+                if (opt.rollover) {
+                    items[n]++
+                }
+            }
+        }
+        j++
+    }
 	return items
 }
 
 func main() {
-	fmt.Println(inc_by_1([1, 0, 1]))
+    vals := []int{1, 0, 0}
+
+    opts := options{rollover: false}
+
+    for n:= 0; n < 900; n++ {
+	    fmt.Println(incby1(vals, opts))
+    }
 }
